5be380083069d725d7e7b0fa68b02bb701a546c1
Edgar Mendez <edgarmendezcruz75@gmail.com>
2024-04-28 04:15:21 -0500

    Actualizacion de componentes, correccion de errores

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\EdgarM\Documents\Codigos\web-monitoreo\.venv
+set VIRTUAL_ENV=C:\Users\EdgarM\Documents\Codigos\hola4\web-monitoreo\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
